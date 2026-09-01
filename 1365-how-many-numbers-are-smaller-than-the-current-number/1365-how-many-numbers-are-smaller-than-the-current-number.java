ae557d67d481cd6f736d5249c598c46a287fa71d
PRANEETH KRS <praneethkrs07@gmail.com>
2026-09-01 15:48:23 +0530

    Time: 7 ms (83.6%), Space: 45.5 MB (67.77%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -4,18 +4,12 @@ class Solution {
         for(int i=0;i<nums.length;i++){
              int count=0;
             for(int j=0;j<nums.length;j++){
-                if(j!=i&&nums[j]<nums[i]){
-                    
+                if(nums[j]<nums[i]){
                     count++;
-    
                 }
             }
             arr[i]=count;
         }
-      
-            
-            
-        
         return arr; 
     }
 }
